1f8f74e5e8b9a6de3183b89a724ab3af3ed76e4c
Matthew McGrattan <matthew.mcgrattan@bodleian.ox.ac.uk>
2018-12-06 09:16:49 +0100

    First commit of Kakadu shell script.

--- kakadu_gen.sh
+++ kakadu_gen.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
 # Placeholder for generating Images using Kakadu
+convert $1 /tmp/temp.tif  # use Imagemagick to convert to a tiff file for clean input to OpenJPEG
+kdu_compress -i /tmp/temp.tif -o test.jp2 Clevels=7 "Cblk={64,64}" Cuse_sop=yes ORGgen_plt=yes ORGtparts=R Corder=RPCL -rate 4 "Cprecincts={256,256},{256,256},{256,256},{128,128},{128,128},{64,64},{64,64},{32,32},{16,16}"
